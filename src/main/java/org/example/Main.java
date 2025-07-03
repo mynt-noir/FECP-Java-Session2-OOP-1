e754471f76a7343f49fc46d543955c10f3d6bd5b
mynt-noir <nuanpatricia.serrano@gcash.com>
2025-07-03 17:25:37 +0800

    Final Submission, Java Unit OOP1 Project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     private static void createAccount(ArrayList<BankAccount> accounts, int accountNumber, String accountName, String answer) {
